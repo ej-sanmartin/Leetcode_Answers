37e95870f47797d4fdc6db24f8018a67d986adfb
Edgar Jr. San Martin <edgar.sanmartinjr@gmail.com>
2020-07-13 07:00:03 -0400

    Create SameTree.cs

---
+++ SameTree.cs
@@ -0,0 +1,26 @@
+/*
+ Given two binary trees, write a function to check if they are the same or not.
+
+ Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+ Definition for a binary tree node.
+ public class TreeNode {
+     public int val;
+     public TreeNode left;
+     public TreeNode right;
+     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+ */
+ 
+public class Solution {
+    public bool IsSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null){ return true; }
+        if(p == null || q == null){ return false; }
+        if(p.val == q.val){ return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right); }
+        return false;
+    }
+}

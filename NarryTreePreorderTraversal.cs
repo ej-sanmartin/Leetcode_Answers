1d1ec0a031cc70c900e59bad5f67ef876f93f005
Edgar Jr. San Martin <edgar.sanmartinjr@gmail.com>
2021-07-01 21:24:15 -0400

    Create NarryTreePreorderTraversal.cs

---
+++ NarryTreePreorderTraversal.cs
@@ -0,0 +1,44 @@
+/*
+    Given the root of an n-ary tree, return the preorder traversal of its nodes' 
+    values.
+
+    T - O(n), n is the amount of nodes in tree. We must traverse through them all to               create the list with the tree nodes in preorder
+    S - O(h), where h is the height of the tree and the amount of recursive call                   stacks we will have
+*/
+
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public IList<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val,IList<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+public class Solution {
+    public IList<int> Preorder(Node root) {
+        List<int> preorderList = new List<int>();
+        preorderHelper(root, preorderList);
+        return preorderList;
+    }
+    
+    private void preorderHelper(Node node, List<int> list){
+        if(node == null) return;
+        
+        list.Add(node.val);
+        
+        foreach(Node child in node.children){
+            preorderHelper(child, list);
+        }
+    }
+}

79ec78f76cd3042149ce76519d2d949f7a4a8610
Edgar Jr. San Martin <edgar.sanmartinjr@gmail.com>
2021-07-03 06:36:57 -0400

    Update RangeSumOfBST.cs

--- RangeSumOfBST.cs
+++ RangeSumOfBST.cs
@@ -1,3 +1,25 @@
+/*
+ UPDATED ANSWER 2021
+ T - O(n) where n at worst case would be the number of nodes in a given tree. Whole tree can be within the range
+ S - O(h), where space depends on the height of the tree which would effect the recursive call stack.
+           Unbalanced - O(n)
+           Balanced - O(nlogn)
+*/
+public class Solution {
+    public int RangeSumBST(TreeNode root, int low, int high) {
+        return RangeSumBSTHelper(root, low, high, 0);
+    }
+    
+    private int RangeSumBSTHelper(TreeNode root, int low, int high, int count){
+        if(root == null) return 0;
+        if(root.val >= low && root.val <= high){
+            return RangeSumBSTHelper(root.left, low, high, count) + RangeSumBSTHelper(root.right, low, high, count) + root.val;
+        }
+        else if(root.val > high) return RangeSumBSTHelper(root.left, low, high, count);
+        else return RangeSumBSTHelper(root.right, low, high, count);
+    }
+}
+
 /**
  Given the root node of a binary search tree, return the sum of
  values of all nodes with value between L and R (inclusive).

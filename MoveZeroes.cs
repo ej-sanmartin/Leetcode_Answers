3177769d6b638ec0bab48da50443d751f457b93d
Edgar Jr. San Martin <edgar.sanmartinjr@gmail.com>
2021-10-08 04:32:26 -0400

    Update MoveZeroes.cs

--- MoveZeroes.cs
+++ MoveZeroes.cs
@@ -18,3 +18,30 @@ public class Solution {
         }
     }
 }
+
+/*
+    Given an integer array nums, move all 0's to the end of it while maintaining 
+    the relative order of the non-zero elements.
+
+    Note that you must do this in-place without making a copy of the array.
+
+    T - O(n), we go through array in one pass
+    S - O(1), we are modifying array in place
+*/
+public class Solution {
+    public void MoveZeroes(int[] nums) {
+        int writePointer = 0;
+        
+        for (int readPointer = 0; readPointer < nums.Length; readPointer++) {
+            if(nums[readPointer] != 0){
+                Swap(nums, writePointer++, readPointer);
+            }
+        }
+    }
+    
+    private void Swap(int[] nums, int a, int b) { 
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+}

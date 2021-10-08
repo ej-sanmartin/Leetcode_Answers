fe340f9bfc98feb4bed9361c25db358ea71dc471
Edgar Jr. San Martin <edgar.sanmartinjr@gmail.com>
2021-10-08 04:38:34 -0400

    Update SortArrayByParity.cs

--- SortArrayByParity.cs
+++ SortArrayByParity.cs
@@ -25,3 +25,32 @@ public class Solution {
         arr[b] = temp;
     }
 }
+
+// Slightly more optimal, at least logic wise
+/*
+    Given an integer array nums, move all the even integers at the 
+    beginning of the array followed by all the odd integers.
+
+    Return any array that satisfies this condition.
+
+    T - O(n), one pass, where n is size of input array
+    S - O(1), all done in place
+*/
+public class Solution {
+    public int[] SortArrayByParity(int[] nums) {
+        int lastEven = 0;
+        for (int i = 0; i < nums.Length; i++) {
+            if(nums[i] % 2 == 0) {
+                Swap(nums, lastEven++, i);
+            }
+        }
+        
+        return nums;
+    }
+    
+    private void Swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+}
